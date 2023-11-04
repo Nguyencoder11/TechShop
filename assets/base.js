79c41e05ce754d1c7e6f4ea0e22d29331f44cd04
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2023-11-04 13:28:10 +0700

    base js

--- assets/base.js
+++ assets/base.js
@@ -34,3 +34,48 @@ document.addEventListener('click', function (event) {
         spanIcon.style.color = 'white';
     }
 });
+
+
+// Menu left
+var mobileNavHasSublist = document.querySelectorAll(".mobile-nav__has-sublist");
+mobileNavHasSublist.forEach(function (item) {
+    var sideMenuBtn = item.querySelector(".mobile-nav__toogle-btn");
+    var sideSubmenu = item.querySelector(".mobile-nav__sublist");
+
+    sideMenuBtn.addEventListener("click", function () {
+        item.classList.toggle("mobile-nav--expanded");
+        // sideSubmenu.style.maxHeight = item.classList.contains("mobile-nav--expanded") ? "700px" : "0";
+
+        var sideMenuOpen = item.querySelector(".mobile-nav__toogle-open");
+        var sideMenuClose = item.querySelector(".mobile-nav__toogle-close");
+        sideMenuBtn.setAttribute("aria-expanded", item.classList.contains("mobile-nav--expanded"));
+
+        if (item.classList.contains("mobile-nav--expanded")) {
+            sideMenuOpen.style.display = "none";
+            sideMenuClose.style.display = "block";
+        } else {
+            sideMenuOpen.style.display = "block";
+            sideMenuClose.style.display = "none";
+        }
+    });
+});
+
+
+// Open and close menu on mobile screeen
+var menuMobileBtn = document.querySelector(".menu-icon-on-mobile");
+var closeMenuIcon = document.querySelector(".drawer__close-button");
+var appContent = document.querySelector(".app");
+var html = document.querySelector('html');
+menuMobileBtn.addEventListener('click', function () {
+    appContent.style.transform = "translateX(300px)";
+    appContent.style.overflow = "hidden";
+    html.classList.add("js-drawer-open", "js-drawer-open-left");
+    document.body.classList.add("js-drawer-open", "js-drawer-open-left");
+});
+
+closeMenuIcon.addEventListener('click', function () {
+    appContent.style.transform = "translateX(0)";
+    appContent.style.overflow = "auto";
+    html.classList.remove("js-drawer-open", "js-drawer-open-left");
+    document.body.classList.remove("js-drawer-open", "js-drawer-open-left");
+});
